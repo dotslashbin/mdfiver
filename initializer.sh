2190c19a98c4448e174cf5c25487665909db6b1b
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-08-17 16:33:31 +0800

    Refactored to use single script
    WIP: removed dependency to md5Mapcreator script
    
    - Implmented cleanup

--- initializer.sh
+++ initializer.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
 
-creator=MD5MapCreator.sh
+# creator=MD5MapCreator.sh
+
+
+
+cleanup_previous_output() {
+	folder=$1
+	file=$2
+
+	rm -f $folder/$file
+}
 
 for folder in $(cat $1); do
-	echo "Copying reator to folder ..."
-	cp -vf ./$creator $folder
-	cd $folder
-	chmod +x $creator
-	./$creator
+	IFS="/" read -a container <<< $folder
+	# echo "Array: ${container[@]}"
+	max=${#container[@]}
+	index="$(($max-1))"
+	# echo "${container[4]}"
+	output="${container[$index]}.md5"
+	echo $folder
+	echo $output
+
+	cleanup_previous_output $folder $output
+	# for file in $(find $folder -type f); do
+		
+	# done
+
+	# cp -vf ./$creator $folder
+	# cd $folder
+	# chmod +x $creator
+	# ./$creator
 done
