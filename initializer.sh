bf3e2408ee51bd291690bd31b8cdc15226c4f316
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-08-17 16:42:46 +0800

    Implemented new implementation

--- initializer.sh
+++ initializer.sh
@@ -11,20 +11,34 @@ cleanup_previous_output() {
 	rm -f $folder/$file
 }
 
+create_ouput_file() {
+	folder=$1
+	file=$2
+
+	touch $folder/$file
+}
+
 for folder in $(cat $1); do
 	IFS="/" read -a container <<< $folder
 	# echo "Array: ${container[@]}"
 	max=${#container[@]}
 	index="$(($max-1))"
 	# echo "${container[4]}"
-	output="${container[$index]}.md5"
-	echo $folder
-	echo $output
+	basename="${container[$index]}"
+	output="$basename.md5"
 
+	# Cleans up the previous folder	
 	cleanup_previous_output $folder $output
-	# for file in $(find $folder -type f); do
-		
-	# done
+
+	#Creates the output file
+	create_ouput_file $folder $output
+	echo "FILE NAME,MD5HASH" >>$folder/$output
+	
+	for file in $(find $folder -type f); do
+		hash=$(md5sum $file | awk '{print $1}')
+		entry="${basename}\\${filename},${hash}"
+		echo $entry >> $folder/$output
+	done
 
 	# cp -vf ./$creator $folder
 	# cd $folder
